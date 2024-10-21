ae4e8c44b6e4b578faefa3d3d20f9750e9933705
Akhil David <akhildavid26@hotmail.com>
2024-10-21 13:41:34 -0700

    removing secret key from backend.tf

--- Section 5 - Remote State Management/terraform-multistate/ec2/backend.tf
+++ Section 5 - Remote State Management/terraform-multistate/ec2/backend.tf
@@ -5,7 +5,7 @@ terraform {
     key    = "ec2demo.tfstate"
     region = "us-west-1"
     access_key = "AKIAJXBRJCPEBT4AUO6A"
-    secret_key = "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"
+    secret_key = ""
     dynamodb_table = "s3-state-lock"
   }
 }
